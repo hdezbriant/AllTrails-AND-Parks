fe7652e5ae90fa4008c554e313df5ca57522491b
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-05 14:56:51 -0700

    Integrated local storage functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 var formEl = document.querySelector('#formEl');
 var searchBar = document.querySelector('#searchBar');
-var lat;
+var lat; 
 var lon;
 var locationiqKey = 'pk.d7efa64ad72930f59e7960a7cb00fa7c';
 var styleKey = 'pk.aa6177ea090cca8fee755da0893b03e8';
@@ -15,7 +15,6 @@ locationiq.key = styleKey;
 
 var favoritesForm = document.querySelector("#favoritesInputForm");
 var favoritesInput = document.querySelector("#favoritesInputText");
-// var favoritesValue = "";
 var parks = ('');
 
 var parksArray = [];
@@ -30,37 +29,21 @@ function init() {
     showParks();
 }
 
-function storeFavoriteParks() {
-    localStorage.setItem("parks", JSON.stringify(parksArray));
+function storeFavoriteParks(favInputText) {
+    var storedParks = JSON.parse(localStorage.getItem("parks"));
+
+    if (storedParks !== null) {
+        parksArray = storedParks;
+
+        parksArray.push(favInputText);
+        localStorage.setItem("parks", JSON.stringify(parksArray));
+    } else {
+        parksArray.push(favInputText);
+        localStorage.setItem("parks", JSON.stringify(parksArray));
+    }
+    parks = parksArray;
 }
 
-// ================
-// Tutor PseudoCode
-// =================
-
-// let parksArray = ["", "park2"];
-// undefined
-// let parksStringify = JSON.stringify(parksArray);
-// undefined
-// parksArray
-// Array["yosemeti", "park2"]
-// parksStringify
-// "[\"yosemeti\",\"park2\"]"
-// localStorage.setItem("parks", parksStringify);
-// undefined
-// let localItems = localStorage.getItem("parks");
-// undefined
-// localItems
-// "[\"yosemeti\",\"park2\"]"
-// let localArray = JSON.parse(localItems);
-// undefined
-// localArray
-// Array["yosemeti", "park2"]
-// localArray[0]
-// "yosemeti"
-
-// Tutor pseudocode
-// ======================
 
 favoritesForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -75,25 +58,28 @@ favoritesForm.addEventListener("submit", function (event) {
     // then runs function to append list item and set local storage
 
     favoritesValue = favoritesInput.value;
-    parksArray.push(favInputText);
-    showParks();
+    // parksArray.push(favInputText);
 
     // Clears input field, stores input in local storage
     favoritesInput.value = "";
     console.log(favInputText);
-    storeFavoriteParks();
+    storeFavoriteParks(favInputText);
+    showParks();
 });
 
 function showParks() {
-    var userFavoritesList = document.getElementById("userPlaceList");
-    var favoritePlace = document.createElement("a");
-    var span = document.createElement("span");
-    var spanIcon = document.createElement("i");
-
-
+    console.log("showParks");
     // Append tree for list item and styling
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    var userFavoritesList = document.getElementById("favoritesList");
+
+    userFavoritesList.innerHTML = "";
+
     for (var i = 0; i < parks.length; i += 1) {
+        var favoritePlace = document.createElement("a");
+        var span = document.createElement("span");
+        var spanIcon = document.createElement("i");
+
         var park = parks[i];
 
         span.appendChild(spanIcon);
@@ -105,91 +91,109 @@ function showParks() {
         spanIcon.setAttribute("aria-hidden", "true");
         userFavoritesList.appendChild(favoritePlace);
         favoritePlace.append(park);
-
-
-        // ================
-        // Tutor suggestion for appending elements
-        // =================
-        // let currentInfo = document.getElementById('panel-block')
-
-        // for (let i = 0; i < currentInfo; i++) {
-        //     userFavoritesList.append(`
-        //       <a> 
-        //         <span></span>
-        //         ${parks[i]}
-        //       </a>
-        //     `)
-        // }
     }
 };
 
 // Local Storage ===============
 
+
 function fetchLocation(query) {
     var url = 'https://us1.locationiq.com/v1/search.php?key=' + locationiqKey + '&q=' + query + '&format=json';
     fetch(url)
         .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            lat = data[0].lat;
-            lon = data[0].lon;
-            console.log(lon, lat)
+        .then((data) => {console.log(data);
+        lat = data[0].lat;
+        lon = data[0].lon;
+        console.log(lon,lat)
+        
+    var url2 = 'https://us1.locationiq.com/v1/nearby.php?key=' + parksKey + '&lat=' + lat + '&lon=' + lon + '&tag=park&radius=3000&format=json'
 
-            var url2 = 'https://us1.locationiq.com/v1/nearby.php?key=' + parksKey + '&lat=' + lat + '&lon=' + lon + '&tag=park&radius=3000&format=json'
+        fetch(url2)
+            .then((response2) => response2.json())
+            .then(function (data2) {
+                console.log(data2);
+                for (i=0;i < data2.length; i+=1) { 
+            parkLat = data2[i].lat;
+            parkLon = data2[i].lon;
+            parkName = data2[i].name;
+        
+            var popup = new mapboxgl.Popup()
+                .setHTML('<b>Park Name:</b>' + parkName);
+                
+            var el = document.createElement('div');
+            el = mapboxgl.Marker
+                el.id = 'markerWithExternalCss';
+                // finally, create the marker
+console.log(parkName);
+            var markerWithExternalCss = new mapboxgl.Marker({color: 'green', rotation: 45 })
+                .setLngLat([parkLon, parkLat])
+                .setPopup(popup)
+                .addTo(map);
+            
+                }
+        } )
 
-            fetch(url2)
-                .then((response2) => response2.json())
-                .then(function (data2) {
-                    console.log(data2);
-                    for (i = 0; i < data2.length; i += 1) {
-                        parkLat = data2[i].lat;
-                        parkLon = data2[i].lon;
-                        parkName = data2[i].name;
-
-                        var popup = new mapboxgl.Popup()
-                            .setHTML('<b>Park Name:</b>' + parkName);
-
-                        var el = document.createElement('div');
-                        el = mapboxgl.Marker
-                        el.id = 'markerWithExternalCss';
-                        // finally, create the marker
-                        console.log(parkName);
-                        var markerWithExternalCss = new mapboxgl.Marker({ color: 'green', rotation: 45 })
-                            .setLngLat([parkLon, parkLat])
-                            .setPopup(popup)
-                            .addTo(map);
-
-                    }
-                })
-
-            var map = new mapboxgl.Map({
-                container: 'map',
-                attributionControl: false, //need this to show a compact attribution icon (i) instead of the whole text
-                style: 'https://tiles.locationiq.com/v3/' + themeStandard + '/vector.json?key=' + styleKey,
-                zoom: 12,
-                center: [lon, lat]
-            });
-
+     var map = new mapboxgl.Map({
+        container: 'map',
+        attributionControl: false, //need this to show a compact attribution icon (i) instead of the whole text
+        style: 'https://tiles.locationiq.com/v3/' + themeStandard + '/vector.json?key='+ styleKey,
+        zoom: 12,
+        center: [lon,lat]
         });
-};
+
+
+        
+        
+
+        //Add Navigation controls to the map to the top-right corner of the map
+        var nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-right');
+        //Add a 'full screen' button to the map
+        map.addControl(new mapboxgl.FullscreenControl());
+        //Add a Scale to the map
+        map.addControl(new mapboxgl.ScaleControl({
+            maxWidth: 80,
+            unit: 'metric' //imperial for miles
+        
+        }));
+        var layerStyles = {
+            "Streets": "streets/vector",
+            "Dark": "dark/vector",
+            "Light": "light/vector"
+        };
+       
+        map.addControl(new locationiqLayerControl({
+            key: locationiq.key,
+            layerStyles: layerStyles
+        }), 'top-left');
+
+        //Add Geolocation control to the map (will only render when page is opened over HTTPS)
+        map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        }));
+    });
+
+}
 
 
 
 function submitHandler(event) {
-                event.preventDefault();
-                var query = searchBar.value.trim();
+    event.preventDefault();
+    var query = searchBar.value.trim();
 
-                if (query) {
-                    fetchLocation(query);
-                    console.log(query);
+    if (query) {
+        fetchLocation(query);
+        console.log(query);
 
-                } else {
-                    alert('Please enter a valid city name');
-                }
-            }
+    } else {
+        alert('Please enter a valid city name');
+    }
+}
 
 
 
 formEl.addEventListener('submit', submitHandler);
-
-    init();
+init();
